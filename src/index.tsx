9ca0461ff477a01d8d528a4f88f2851f9bb40e04
ganfubin <1209711358@qq.com>
2022-01-17 11:47:25 +0800

    feat: ts-loader

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-// import 'antd/dist/antd.less'
+import 'antd/dist/antd.less'
 import {ConfigProvider} from 'antd'
 import zhCN from 'antd/lib/locale/zh_CN'
 import App from './App'
